f3c94c22f7f5d85726ec714a2e14db979bb15ddc
Leymoy24 <ivan_d03@mail.ru>
2024-02-10 00:21:46 +0300

    Add Top ToolBar

--- app/src/main/java/com/example/movie_poster/presentation/MainActivity.kt
+++ app/src/main/java/com/example/movie_poster/presentation/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.movie_poster.presentation
 
+import android.R
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.movie_poster.databinding.ActivityMainBinding
+import com.google.android.material.appbar.MaterialToolbar
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -11,5 +14,12 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+//        val toolbar = findViewById<MaterialToolbar>(R.id.)
+//        toolbar.setNavigationIcon(R.drawable.ic_your_icon)
+//        toolbar.setNavigationOnClickListener {
+//            // Ваш код обработки щелчка на иконке
+//        }
+
     }
 }
